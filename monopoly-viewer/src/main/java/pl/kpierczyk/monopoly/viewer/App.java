fafb43232d66401eae08b93945d174373d1b657d
Krzysztof Pierczyk <krzysztof.pierczyk@gmail.com>
2018-11-27 10:40:51 +0100

    Maven environment preparation (basic POM files and project structure)

---
+++ monopoly-viewer/src/main/java/pl/kpierczyk/monopoly/viewer/App.java
@@ -0,0 +1,13 @@
+package pl.kpierczyk.monopoly.viewer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
