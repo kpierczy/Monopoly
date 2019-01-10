07919ff3929eeac9a8d8b0d6ad2a299f533b5465
kpierczy <36889329+kpierczy@users.noreply.github.com>
2019-01-10 18:58:42 +0100

    Przypominajka

--- monopoly-app/src/main/java/pl/kpierczyk/monopoly/app/App.java
+++ monopoly-app/src/main/java/pl/kpierczyk/monopoly/app/App.java
@@ -10,6 +10,7 @@ public class App
 {
     public static void main( String[] args )
     {
+        /*!!!!!!!!!!!!!!!!!ZMIEŃ ŚCIEŻKĘ NA WZGLĘDNĄ!!!!!!!!!!!!!!!!!!*/
         Model model  = new Model("F:\\VS Code Projects\\Java\\Monopoly\\monopoly-model\\src\\main\\java\\pl\\sources\\config.txt");
         Viewer view = new View(model);
         Controller controller = new Controller(model, view);
